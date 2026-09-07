e106c7d5593f9880070cb07095882c278d82c443
Lorem Dev <contact@lorem.dev>
2026-09-07 21:54:28 +0200

    feat: report which ggml backends a binary loaded

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -27,6 +27,7 @@ export type { InstallSherpaOptions } from './provision/sherpaInstall.js';
 export { NodeFs } from './system/nodeFs.js';
 export { SystemClock, UlidIds } from './system/systemClock.js';
 export { cpuTopology, parsePerformanceCores } from './system/cpuTopology.js';
+export { parseBackends, probeBackends } from './system/accelerator.js';
 
 export { FfmpegAudioTool } from './audio/ffmpeg.js';
 
